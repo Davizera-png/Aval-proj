d05eeba381492065dfd0dbdf73b8f3f10227b358
Davizera-png <olidavidson34@gmail.com>
2020-05-15 19:26:51 -0300

    Cabecalho

--- src/prog.java
+++ src/prog.java
@@ -3,7 +3,11 @@ public class prog {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("SISTEMA DE PERFIL DO CLIENTE");
+		System.out.println("------------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		
 	}
 
 }
