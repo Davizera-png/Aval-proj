90c9728cb0389b245433c7a263f1e6f8c7fe7e36
Davizera-png <olidavidson34@gmail.com>
2020-05-15 19:55:12 -0300

    Leitura dos outros dados

--- src/prog.java
+++ src/prog.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
+import java.util.Locale;
 public class prog {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
-		int compras;
-		double ticket;
+		double medticket;
+		int compras, atrasos;
+		char pagamento;
 		
-		//leitura de dados
+		//inicio do cabecalho
 		
 		System.out.println("SISTEMA DE PERFIL DO CLIENTE");
 		System.out.println("------------------------------------");
@@ -17,10 +20,24 @@ public class prog {
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 		//fim do cabecalho
 		
+		//inicio da leitura do volume
 		System.out.printf("Quantas compras o cliente fez no último ano? ");
 		compras = sc.nextInt();
 		System.out.printf("Qual o ticket médio? ");
-		ticket = sc.nextDouble();
+		medticket = sc.nextDouble();
+		
+		
+		
+		//inicio da leitura de outros dados
+		
+		System.out.printf("Quantas vezes o cliente atrasou o pagamento? ");
+		atrasos = sc.nextInt();
+		
+		System.out.printf("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
+		pagamento = sc.next().charAt(0);
+		
+		//fim da leitura dos outros dados
+		
 		
 		sc.close();
 	}
