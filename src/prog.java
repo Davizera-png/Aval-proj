caef31d9274b23737dd902d06eeb0ab8d3a4ed4e
Davizera-png <olidavidson34@gmail.com>
2020-05-15 19:36:47 -0300

    Leitura do volume

--- src/prog.java
+++ src/prog.java
@@ -1,13 +1,28 @@
-
+import java.util.Scanner;
 public class prog {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		
+		int compras;
+		double ticket;
+		
+		//leitura de dados
+		
 		System.out.println("SISTEMA DE PERFIL DO CLIENTE");
 		System.out.println("------------------------------------");
+		System.out.println("");
+		
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		//fim do cabecalho
 		
+		System.out.printf("Quantas compras o cliente fez no último ano? ");
+		compras = sc.nextInt();
+		System.out.printf("Qual o ticket médio? ");
+		ticket = sc.nextDouble();
 		
+		sc.close();
 	}
 
 }
